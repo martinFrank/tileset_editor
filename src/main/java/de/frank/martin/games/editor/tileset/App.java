c9d4e7024f5a25d0cd033179a20ccad5226c77de
martin.frank <martin.frank.private@gmail.com>
2019-03-28 10:11:37 +0100

    initial setup

---
+++ src/main/java/de/frank/martin/games/editor/tileset/App.java
@@ -0,0 +1,11 @@
+package de.frank.martin.games.editor.tileset;
+
+import de.elite.games.cli.CommandLineInterpreter;
+
+class App {
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/test/java/de/frank/martin/games/editor/tileset/DrawTest.java
@@ -0,0 +1,14 @@
+package de.frank.martin.games.editor.tileset;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DrawTest {
+
+
+    @Test
+    public void drawOverTest() {
+        Assert.fail();
+    }
+
+}
