a9f1f141263161046fe6fa0fff22a5a40e1fbebf
martin.frank <martin.frank.private@gmail.com>
2019-04-03 18:15:04 +0200

    added rastering for tileset (initial only)

--- src/main/java/de/elite/games/editor/tileset/TilesetController.java
+++ src/main/java/de/elite/games/editor/tileset/TilesetController.java
@@ -2,13 +2,17 @@ package de.elite.games.editor.tileset;
 
 import de.elite.games.editor.tileset.tileset.Tileset;
 import de.elite.games.editor.tileset.tileset.TilesetFactory;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import org.slf4j.Logger;
@@ -16,6 +20,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TilesetController {
 
@@ -23,12 +29,27 @@ public class TilesetController {
 
     private final TilesetFactory tilesetFactory = new TilesetFactory();
     private Tileset selected;
+    private File lastVisitedDirectory = new File(".");
 
     @FXML
     private ListView<Tileset> tilesetList;
 
     @FXML
     private Button addTilesetButton;
+
+    @FXML
+    private TextField tilesetNameText;
+    @FXML
+    private TextField tilesetIdText;
+    @FXML
+    private TextField tilesetWidthText;
+    @FXML
+    private TextField tilesetHeightText;
+
+    @FXML
+    private ImageView tilesetImageView;
+    @FXML
+    private Pane tilesetImagePane;
 
 
     @FXML
@@ -37,12 +58,14 @@ public class TilesetController {
         LOGGER.debug("add tileset button pressed ");
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Tileset Image File");
-        fileChooser.setInitialDirectory(new File("."));
+        fileChooser.setInitialDirectory(lastVisitedDirectory);
         File file = fileChooser.showOpenDialog(owner);
         if (file != null) {
             LOGGER.debug("opened file: {}", file);
+            lastVisitedDirectory = file.getParentFile();
             try {
                 Tileset tileset = tilesetFactory.fromFile(file);
+                selectTileset(tileset);
                 tilesetList.getItems().add(tileset);
             } catch (IOException e) {
                 AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Tileset creation error!", "Error creating new Tileset");
@@ -55,15 +78,34 @@ public class TilesetController {
     }
 
     void initListListener() {
-        tilesetList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tileset>() {
-            @Override
-            public void changed(ObservableValue<? extends Tileset> observable, Tileset oldValue, Tileset newValue) {
-                LOGGER.debug("oldValue: {} ", oldValue);
-                LOGGER.debug("newValue: {} ", newValue);
-                selected = newValue;
+        tilesetList.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> selectTileset(newValue));
+    }
 
-            }
-        });
+    private void selectTileset(Tileset selected) {
+        this.selected = selected;
+        tilesetNameText.setText(selected.getName());
+        tilesetIdText.setText(selected.getId());
+        tilesetWidthText.setText("" + selected.getWidth());
+        tilesetHeightText.setText("" + selected.getHeight());
+        tilesetImageView.setImage(selected.getImage());
+        tilesetImageView.setFitHeight(selected.getImageHeight());
+        tilesetImageView.setFitWidth(selected.getImageWidth());
+        tilesetImagePane.getChildren().clear();
+        tilesetImagePane.getChildren().add(tilesetImageView);
+        List<Rectangle> rectangles = createRectangles();
+        tilesetImagePane.getChildren().addAll(rectangles);
 
+    }
+
+    private List<Rectangle> createRectangles() {
+        List<Rectangle> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Rectangle rectangle = new Rectangle(i * 32, 0, 32, 32);
+            rectangle.setFill(null);
+            rectangle.setStroke(Color.GREEN);
+            list.add(rectangle);
+        }
+        return list;
     }
 }
--- src/main/java/de/elite/games/editor/tileset/tileset/Tileset.java
+++ src/main/java/de/elite/games/editor/tileset/tileset/Tileset.java
@@ -10,16 +10,68 @@ public class Tileset {
     private int width;
     private int height;
     private transient Image image;
+    private double imageWidth;
+    private double imageHeight;
 
     public Tileset(String name, String id, byte[] bytes, Image image) {
         this.name = name;
         this.id = id;
         this.imageBytes = bytes;
-        this.image = image;
+        setImage(image);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Image getImage() {
+        return image;
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    private void setImage(Image image) {
+        this.image = image;
+        imageWidth = image.getWidth();
+        imageHeight = image.getHeight();
+    }
+
+    public double getImageWidth() {
+        return imageWidth;
+    }
+
+    public double getImageHeight() {
+        return imageHeight;
     }
 }
